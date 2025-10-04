dabea85a91679c29cf03c950cc53af55eed3cd06
John Bell <jan@altenter.org>
2025-10-04 15:05:06 +0200

    ADD forever loop

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module RaspberryWeather
 go 1.25
 
 require (
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	periph.io/x/conn/v3 v3.7.2
 	periph.io/x/devices/v3 v3.7.4
 	periph.io/x/host/v3 v3.8.5
 )
+
+require golang.org/x/sys v0.29.0 // indirect
--- main.go
+++ main.go
@@ -4,21 +4,51 @@ import (
 	"RaspberryWeather/temperature"
 	"fmt"
 	"time"
+
+	"github.com/eiannone/keyboard"
 )
+
+var done bool
 
 func main() {
 
+	fmt.Printf("Start Weather App\n")
+	done = false
+
+	go getKey()
+
+	counter := 0
+	interval := 20
+
 	var sensor temperature.Bmp280
-
 	sensor.Initialize(temperature.DefaultIc2TempSensorAddr)
-
-	for i := 0; i < 10; i++ {
-		sensor.UpdateSensorData()
-
-		fmt.Printf("Temperatur: %.2f °C\n", sensor.Temperature)
-		fmt.Printf("Druck: %.2f hPa\n", sensor.Pressure)
-		time.Sleep(2 * time.Second)
+	for !done {
+		if counter%interval == 0 {
+			counter = 0
+			fmt.Println("----------------------")
+			fmt.Println(time.Now().Format(time.RFC3339))
+			sensor.UpdateSensorData()
+			fmt.Printf("Temperatur: %.2f °C\n", sensor.Temperature)
+			fmt.Printf("Druck: %.2f hPa\n", sensor.Pressure)
+			fmt.Println("")
+		}
+		time.Sleep(1 * time.Second)
+		counter++
 	}
 
 	sensor.Uninitialize()
+	fmt.Printf("goodbye\n")
+}
+
+func getKey() {
+	if err := keyboard.Open(); err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
+	_, _, err := keyboard.GetKey()
+	if err != nil {
+	}
+	fmt.Println("got key")
+	done = true
 }
